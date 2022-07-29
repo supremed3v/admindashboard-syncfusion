1a10facac7a4ac9ae61cbbd9f17b7ccb8a4f2f1b
supremed3v <saadsiddiqui-programmer@protonmail.com>
2022-07-29 16:29:31 +0500

    Added Pages folder

---
+++ src/components/Charts/SparkLine.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SparkLine = () => {
+  return <div>Line</div>;
+};
+
+export default SparkLine;
---
+++ src/pages/Charts/Line.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Line = () => {
+  return <div>LineChart</div>;
+};
+
+export default Line;
